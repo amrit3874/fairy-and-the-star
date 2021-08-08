f5545ffb73d61e49c383560cb7403d5b2c4f7e1a
amrit3874 <77009260+amrit3874@users.noreply.github.com>
2021-08-08 11:40:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,6 +13,7 @@ function preload()
 	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
 	bgImg = loadImage("images/starNight.png");
 	fairyVoice = loadSound("sound/JoyMusic.mp3");
+}
 
 //ddufbdghffkf fgjgdjefefgngndhignehiengoenfkf fjffkffkf,g9egjmwigjneignwigwngiwjngfjf fkdmfgkgkghgungmbjgkerdilskeongmdhfkeielfeffeff
 function setup() {
